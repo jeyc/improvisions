b70c2308fc740865778b242186505f18fc4d5f58
biouche <jerome.eychenne@gmail.com>
2015-05-25 23:35:42 +0200

    Homepage vide

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerome
+ * Date: 25/05/2015
+ * Time: 21:42
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller{
+
+    /**
+     * @Route("/")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction() {
+        return $this->render('index.html.twig');
+    }
+
+}
