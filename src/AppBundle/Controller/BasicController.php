c819d9fcc713a3c42b632cf3bb7928edf6bcb29b
biouche <jerome.eychenne@gmail.com>
2015-05-26 13:45:22 +0200

    BasicController + base template : gestion des messages flash.

--- src/AppBundle/Controller/BasicController.php
+++ src/AppBundle/Controller/BasicController.php
@@ -37,4 +37,34 @@ class BasicController extends Controller {
         return $this->get('translator')->trans($message, $param, $domain, $locale);
     }
 
+    /**
+     * @param $type
+     * @param $message
+     */
+    protected function flash($type, $message, $param = array(), $domain = null, $locale = null)
+    {
+        $str = $this->translate($message, $param, $domain, $locale);
+
+        $this->get('session')->getFlashBag()->add(
+            $type,
+            $str
+        );
+    }
+
+    /**
+     * @param $message
+     */
+    protected function flashNotice($message, $param = array(), $domain = null, $locale = null)
+    {
+        $this->flash('notice', $message, $param, $domain, $locale);
+    }
+
+    /**
+     * @param $message
+     */
+    protected function flashError($message, $param = array(), $domain = null, $locale = null)
+    {
+        $this->flash('error', $message, $param, $domain, $locale);
+    }
+
 }
